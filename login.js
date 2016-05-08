83e8a6bac97829ee56509163b883996575be58ff
Flying_Apple_Pie <s92025592025@gmail.com>
2016-05-08 15:28:14 -0700

    account login feature implemented
    can't login if there is your account information in the xml file, but
    now it will only direct the page to google if the account logged in
    successfully

--- login.js
+++ login.js
@@ -1,6 +1,8 @@
 (function(){
 	window.onload = function(){
 		document.getElementById("login_btn").onclick = verify;
+		document.getElementById("account_input").onkeypress = submit;
+		document.getElementById("pw_input").onkeypress = submit;
 	};
 
 	function verify(){
@@ -23,15 +25,25 @@
 			var request = new XMLHttpRequest();
 			request.onload = function(){
 				var response = this.responseText;
+				console.log(response);
 				if(response == "TRUE"){
-					window.location = "www.google.com";
+					window.location = "http://www.google.com";
 				}else{
 					alert("Wrong account or password");
 				}
 			};
 
-			request.open("GET", "data/loginTest.php", false);
+			request.open("GET", "data/loginTest.php?name=" + 
+				document.getElementById("account_input").value + "&pw=" + 
+				document.getElementById("pw_input").value, false);
 			request.send();
 		}
 	}
+
+	function submit(e){
+		if(e.keycode == 13 || e.which == 13){
+			verify();
+		}
+	}
+
 })();
