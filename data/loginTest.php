94a70f98dba37557b0f220765cfa5bd8cd2083a5
s92025592025 <s92025592025@gmail.com>
2016-06-09 09:54:14 -0700

    Will remember user has logged in

--- data/loginTest.php
+++ data/loginTest.php
@@ -1,9 +1,12 @@
 <?php
+	session_start();
 	header("Content-Type: type/plain");
 	$name = null;
 	$pw = null;
 
-	if($_GET["name"] && $_GET["pw"]){
+	if(isset($_GET["check"])){
+		print(checkLoggedIn());
+	}else if($_GET["name"] && $_GET["pw"]){
 		$name = $_GET["name"];
 		$pw = $_GET["pw"];
 		print(varify($name, $pw));
@@ -19,10 +22,21 @@
 		foreach($accounts as $account){
 			if ($account->getElementsByTagName("name")->item(0)->nodeValue == $name) {
 				if($account->getElementsByTagName("password")->item(0)->nodeValue == $pw){
+					$_SESSION["user"] = $name;
 					return "TRUE";
 				}
+				unset($_SESSION["user"]);
 				return "FALSE";
 			}
+		}
+
+		unset($_SESSION["user"]);
+		return "FALSE";
+	}
+
+	function checkLoggedIn(){
+		if(isset($_SESSION["user"])){
+			return "TRUE";
 		}
 
 		return "FALSE";
--- login.js
+++ login.js
@@ -1,4 +1,8 @@
 (function(){
+	"use strict";
+
+	checkLoggedIn();
+
 	window.onload = function(){
 		document.getElementById("login_btn").onclick = verify;
 		document.getElementById("account_input").onkeypress = submit;
@@ -7,15 +11,15 @@
 
 	function verify(){
 		if(document.getElementById("account_input").value == ""){
-			document.getElementById("account_label").innerHTML += 
-				"<span>*Please enter your account name</span>";
+			document.getElementById("account_label").innerHTML = 
+				"Account: <span>*Please enter your account name</span>";
 		}else{
 			document.getElementById("account_label").innerHTML = "Account: ";
 		}
 
 		if(document.getElementById("pw_input").value == ""){
-			document.getElementById("pw_label").innerHTML += 
-			 "<span>*Please enter your account password</span>";
+			document.getElementById("pw_label").innerHTML = 
+			 "Password: <span>*Please enter your account password</span>";
 		}else{
 			document.getElementById("pw_label").innerHTML = "Password: ";
 		}
@@ -44,6 +48,19 @@
 		if(e.keycode == 13 || e.which == 13){
 			verify();
 		}
+	}
+
+	function checkLoggedIn(){
+		var request = new XMLHttpRequest();
+		request.onload = function(){
+			console.log(this.responseText);
+			if(this.responseText == "TRUE"){
+				window.location = "http://www.w3schools.com/js/js_window_location.asp";
+			}
+		};
+
+		request.open("GET", "data/loginTest.php?check=ivs630s5573v56dj9a", false);
+		request.send();
 	}
 
 })();
