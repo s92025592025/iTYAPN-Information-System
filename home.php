3d92aba3b568c1288e10c71181e5180bbe9a8a14
s92025592025 <s92025592025@gmail.com>
2016-08-02 08:57:46 +0800

    fiinshed home dashboard layout and ticket display
    it can only display the tickets, still can't see details

--- home.php
+++ home.php
@@ -10,15 +10,40 @@
 ?>
 
 	<div class="container">
-	<h1>Welcome, <?=$_SESSION["user"]?></h1>
+		<div>
+			<h1>Welcome, <?=$_SESSION["user"]?> <button type="button" id="new_ticket_btn" class="btn btn-primary">New Tickets</button></h1>
+		</div>
 		<fieldset>
 			<legend>My Tickets</legend>
 			<?=myTickets()?>
+		</fieldset>
+		<fieldset>
+			<legend>Tickets in queue</legend>
+			<?=activeTickets()?>
+		</fieldset>
+		<fieldset>
+			<legend>Past tickets</legend>
+			<?=pastTickets()?>
 		</fieldset>
 	</div>
 
 <?php
 	HTMLFooter();
+
+	function makeTable($tickets){
+		?>
+		<table>
+			<tr><th>#Id</th><th>Company</th><th>Owner</th><th>Status</th></tr>
+			<?php
+				foreach($tickets as $ticket){
+					?>
+						<tr><td><?=$ticket["id"]?></td><td><?=$ticket["c_name"]?></td><td><?=$ticket["user"]?></td><td><?=$ticket["status"]?></td></tr>
+					<?php
+				}
+			?>
+		</table>
+		<?php
+	}
 
 	function myTickets(){
 		$conn = connectToDB();
@@ -26,23 +51,52 @@
 		$user = $conn->quote($_SESSION["user"]);
 		$year = $conn->quote(date("Y"));
 
-		$tickets = $conn->query("SELECT *
+		$tickets = $conn->query("SELECT dbo.ticket.id, dbo.company_list.c_name, 
+									dbo.company_list.e_name, dbo.user_data.[user], dbo.ticket.status
 								FROM dbo.ticket
 								JOIN dbo.user_data ON dbo.ticket.user_id = dbo.user_data.id
 								JOIN dbo.company_list ON dbo.company_list.id = dbo.ticket.company_id
-								WHERE dbo.user_data.[user] = $user AND dbo.ticket.year = $year
+								WHERE dbo.user_data.[user] = $user AND dbo.ticket.year LIKE $year
+									AND dbo.ticket.status != 'success' 
+									AND dbo.ticket.status != 'fail'
 								ORDER BY dbo.ticket.company_id");
-		?>
-		<table>
-			<tr><th>#Id</th><th>Company</th><th>Owner</th><th>Status</th></tr>
-			<?php
-				foreach($tickets as $ticket){
-					?>
-						<tr><td><?=$ticket["dbo.ticket.id"]?></td><td><?=$ticket["dbo.company_list.e_name"]?></td><td></td><?=$ticket["dbo.ticket.status"]?></td></tr>
-					<?php
-				}
-			?>
-		</table>
-		<?php
+
+		makeTable($tickets);
+	}
+
+	function activeTickets(){
+		$conn = connectToDB();
+
+		$user = $conn->quote($_SESSION["user"]);
+		$year = $conn->quote(date("Y"));
+
+		$tickets = $conn->query("SELECT dbo.ticket.id, dbo.company_list.c_name, 
+									dbo.company_list.e_name, dbo.user_data.[user], dbo.ticket.status
+								FROM dbo.ticket
+								JOIN dbo.user_data ON dbo.ticket.user_id = dbo.user_data.id
+								JOIN dbo.company_list ON dbo.company_list.id = dbo.ticket.company_id
+								WHERE dbo.user_data.[user] != $user AND dbo.ticket.year LIKE $year
+									AND dbo.ticket.status != 'success'
+									AND dbo.ticket.status != 'fail'
+								ORDER BY dbo.ticket.company_id");
+
+		makeTable($tickets);
+	}
+
+	function pastTickets(){
+		$conn = connectToDB();
+
+		$year = $conn->quote(date("Y"));
+
+		$tickets = $conn->query("SELECT dbo.ticket.id, dbo.company_list.c_name, 
+									dbo.company_list.e_name, dbo.user_data.[user], dbo.ticket.status
+								FROM dbo.ticket
+								JOIN dbo.user_data ON dbo.ticket.user_id = dbo.user_data.id
+								JOIN dbo.company_list ON dbo.company_list.id = dbo.ticket.company_id
+								WHERE dbo.ticket.year < $year OR dbo.ticket.status = 'success'
+									OR dbo.ticket.status = 'fail'
+								ORDER BY dbo.ticket.company_id");
+
+		makeTable($tickets);
 	}
 ?>
