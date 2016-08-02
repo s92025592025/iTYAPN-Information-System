91494eedf664937c7fed2ddb59a484fa9b76932e
s92025592025 <s92025592025@gmail.com>
2016-08-03 07:59:08 +0800

    finished first hand data checking

--- account.js
+++ account.js
@@ -5,18 +5,44 @@
 		document.getElementById("submit").onclick = changePw;
 	};
 
+	// pre: when the user pressed the submit button
+	// post: start changing password process
 	function changePw(){
-		if(document.getElementById("new_pw").textValue != 
-			document.getElementById("re_enter_pw").textValue){
+		if(document.getElementById("new_pw").value != 
+			document.getElementById("re_enter_pw").value){
 			// if new password can't be entered the same twice
-			alert("Re-enter not the same");
-		}else if(document.getElementById("old_pw").textValue.length == 0 ||
-					document.getElementById("new_pw").textValue.length = 0 ||
-					document.getElementById("re_enter_pw").textValue == 0){
+
+			// make the boxes red
+			document.getElementById("new_pw").classList.add("error");
+			document.getElementById("re_enter_pw").classList.add("error");
+			// show error message
+			document.getElementById("change_pw_warning").innerHTML = "Re-enter not the same";
+			document.getElementById("old_pw_warning").innerHTML = "";
+		}else if(document.getElementById("old_pw").value.length == 0 ||
+					document.getElementById("new_pw").value.length == 0 ||
+					document.getElementById("re_enter_pw").value.length == 0){
 			// if any of the input is blank
-			
+			checkBlankInput("#change_pw");
+
+			// show error message
+			document.getElementById("change_pw_warning").innerHTML = "";
+			document.getElementById("old_pw_warning").innerHTML = "The red box below shouldn't be blank";
 		}else{
+			document.getElementById("change_pw_warning").innerHTML = "";
+			document.getElementById("old_pw_warning").innerHTML = "";
+
 			var request = new XMLHttpRequest();
+		}
+	}
+
+	function checkBlankInput(block){
+		var inputs = document.querySelectorAll(block + " input");
+		for(var i = 0; i < inputs.length; i++){
+			if(inputs[i].value.length == 0){
+				inputs[i].classList.add("error");
+			}else{
+				inputs[i].classList.remove("error");
+			}
 		}
 	}
 
--- account.php
+++ account.php
@@ -11,13 +11,15 @@
 
 	<div class="container">
 		<h1>Account Info</h1>
-		<form>
+		<div id="change_pw">
 			<legend>Change passowrd</legend>
+			<p id="old_pw_warning" class="warning"></p>
 			<label>old password: <input type="password" id="old_pw"></label>
+			<p id="change_pw_warning" class="warning"></p>
 			<label>new password: <input type="password" id="new_pw"></label>
 			<label>re-enter new pw: <input type="password" id="re_enter_pw"></label>
 			<button id="submit" class="btn btn-primary">Submit</button>
-		</form>
+		</div>
 	</div>
 
 	<?php
