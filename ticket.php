28076b3aecad9c8cb8b57332575ca89c4538c8d8
s92025592025 <s92025592025@gmail.com>
2016-08-09 10:08:40 +0800

    data from query can't properly fetched, show D for everything

--- ticket.php
+++ ticket.php
@@ -8,9 +8,28 @@
 
 	if(!isset($_GET["id"])){
 		header("Location: home.php");
+		die();
 	}else{
 		HTMLHeader("Ticket #".$_GET["id"], "ticket.css", "");
 
+		$conn = connectToDB("data/dbInformation.txt");
+		$ticketId = $conn->quote($_GET["id"]);
+		$ticketInfo = $conn->query("SELECT TOP(1) [user], c_name, e_name, contactee,
+										dbo.ticket.email, [address], [status],
+										phone
+									FROM dbo.ticket
+									JOIN dbo.company_list ON company_id = dbo.company_list.id
+									JOIN dbo.user_data ON user_id = dbo.user_data.id
+									WHERE dbo.ticket.id LIKE $ticketId");
+
+		$info = $ticketInfo->fetchColumn();
+		?>
+
+		<div id="main" class="container">
+			<h1><?=$info["phone"]?></h1>
+		</div>
+
+		<?php
 		HTMLFooter();
 	}
 
