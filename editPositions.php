5993149aaf9129c905c00e2b794815a6b61f6d4e
s92025592025 <s92025592025@gmail.com>
2016-08-26 08:29:07 +0800

    filter out the unusual case when load in the page

--- editPositions.php
+++ editPositions.php
@@ -4,4 +4,27 @@
 	checkLoggedIn();
 
 	session_start();
+
+	if($_SERVER["REQUEST_METHOD"] == "POST"){
+		editPosition();
+	}else{
+		displayPage();
+	}
+?>
+
+<?php
+	# this is a function bolck
+
+	# pre: when the user tried to delete or add a new position
+	# post: add or delete the position in xml
+	function editPosition(){};
+
+	# pre: when the user request to edit the positions
+	# post: display a page for the user to edit the positions in the ticket
+	function displayPage(){
+		if(!isset($_GET["id"]) || !is_numeric($_GET["id"])){
+			showErrorMessage();
+			die();
+		}
+	};
 ?>
