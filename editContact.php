659a87e7082312d3976b0c154b2ab71264aebd76
s92025592025 <s92025592025@gmail.com>
2016-08-29 10:29:17 +0800

    layout finished

--- editContact.php
+++ editContact.php
@@ -32,6 +32,31 @@
 		?>
 			<div class="container">
 				<h1>編輯聯絡人 Edit Contacts</h1>
+				<form class="form-horizontal" action="editContact.php" method="POST">
+					<div class="form-group">
+						<label class="control-label col-sm-2">聯絡人: </label>
+						<div class="col-sm-6">
+							<input class="form-control" name="contactee" placeholder="請輸入聯絡人姓名">
+						</div>
+					</div>
+					<div class="form-group">
+						<label class="control-label col-sm-2">電子信箱: </label>
+						<div class="col-sm-6">
+							<input class="form-control" type="email" name="email" placeholder="請輸入聯絡人電子信箱地址">
+						</div>
+					</div>
+					<div class="form-group">
+						<label class="control-label col-sm-2">連絡電話: </label>
+						<div class="col-sm-6">
+							<input class="form-control" type="tel" name="phone" placeholder="請輸入聯絡人電話">
+						</div>
+					</div>
+					<div class="form-group">
+						<div class="col-sm-offset-2 col-sm-10">
+							<button type="submit" class="btn btn-primary">修改聯絡人資料 Edit Contact</button>
+						</div>
+					</div>
+				</form>
 			</div>
 		<?php
 		HTMLFooter();
