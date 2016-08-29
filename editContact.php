0827f6187daf9103aa68028151a92d9e27072f59
s92025592025 <s92025592025@gmail.com>
2016-08-29 17:36:50 +0800

    can now edit contacts in tickets now

--- editContact.php
+++ editContact.php
@@ -18,7 +18,53 @@
 
 	# pre: when a post request is made
 	# post: update the contact info in db
-	function editContact(){}
+	function editContact(){
+		if(!isset($_POST["ticket_id"]) || !is_numeric($_POST["ticket_id"])){
+			showErrorMessage();
+			die();
+		}
+
+		$conn = connectToDB("data/dbInformation.txt");
+
+		$id = $conn->quote($_POST["ticket_id"]);
+		$contactee = nullMaker($conn, $_POST["contactee"]);
+		$email = nullMaker($conn, $_POST["email"]);
+		$phone = nullMaker($conn, $_POST["phone"]);
+
+		if($conn->query("UPDATE dbo.ticket
+						SET contactee = $contactee, [email] = $email, c_phone = $phone
+						WHERE [id] = $id")){
+			$ticket = new DOMDocument();
+			$ticket->load("data/tickets/".$_POST["ticket_id"].".xml");
+			$log = $ticket->createElement("log");
+			$log->setAttribute("time", microtime(true));
+			$log->setAttribute("author", "system");
+			$log->setAttribute("status", "Comment");
+			$log->appendChild($ticket->createElement("text", $_SESSION["user"]." has changed contact information"));
+			$log->appendChild($ticket->createElement("files"));
+			$ticket->getElementsByTagName("logs")->item(0)->appendChild($log);
+			if($ticket->save("data/tickets/".$_POST["ticket_id"].".xml")){
+				header("Location: ticket.php?id=".$_POST["ticket_id"]);
+				die();
+			}else{
+				showErrorMessage();
+				die();
+			}
+		}else{
+			showErrorMessage();
+			die();
+		}
+	}
+
+	# pre: when the data passed in might be null
+	# post: make the data null fit in sql if null, quote it when not null
+	function nullMaker($conn, $data){
+		if($data == null){
+			return 'NULL';
+		}
+
+		return $conn->quote($data);
+	}
 
 	# pre: when the user wants to edit contact info
 	# post: show a pagw with previous contact info
@@ -78,6 +124,7 @@
 					</div>
 					<div class="form-group">
 						<div class="col-sm-offset-2 col-sm-10">
+						<input type="hidden" name="ticket_id" value="<?=$_GET["id"]?>">
 							<button type="submit" class="btn btn-primary">修改聯絡人資料 Edit Contact</button>
 						</div>
 					</div>
