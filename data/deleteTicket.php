04e408e195dd23e67acfabbe3b3313880c874b58
s92025592025 <s92025592025@gmail.com>
2016-08-30 10:15:53 +0800

    can now delete ticket in sql and xml successfully

--- data/deleteTicket.php
+++ data/deleteTicket.php
@@ -4,4 +4,26 @@
 	checkLoggedIn();
 
 	session_start();
+
+	if(!isset($_POST["id"]) || !is_numeric($_POST["id"])){
+		showErrorMessage();
+		die();
+	}
+
+	$conn = connectToDB("dbInformation.txt");
+
+	$id = $conn->quote($_POST["id"]);
+	
+	if($conn->query("DELETE FROM dbo.ticket WHERE [id] = $id")){
+		if(unlink("tickets/".$_POST["id"].".xml")){
+			header("Location: ../home.php");
+			die();
+		}else{
+			showErrorMessage();
+			die();
+		}
+	}else{
+		showErrorMessage();
+		die();
+	}
 ?>
