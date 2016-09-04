7ddfb8207f175b0eb1b509b19b38afe78887817c
s92025592025 <s92025592025@gmail.com>
2016-09-04 10:24:13 +0800

    little update

--- data/test.php
+++ data/test.php
@@ -1,6 +1,7 @@
 <?php
 	$curl = curl_init();
 	curl_setopt($curl, CURLOPT_URL, "https://api.imgur.com/3/credits.xml?_fake_status=200");
+	curl_setopt($curl, CURLOPT_HTTPHEADER, array("Authorization: Client-ID "));
 	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 
